050fe925164e7bc3442717eb9c723d1ba2a948c2
LincolnFaradey <lyfaradey@yahoo.com>
2016-04-29 14:15:42 -0400

    Initial commit

--- Dcoin lite/ViewController.swift
+++ Dcoin lite/ViewController.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    @IBOutlet weak var placeholderView: UIView!
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView = WKWebView(frame: view.frame)
+        webView.center = view.center
+        placeholderView.addSubview(webView)
+        
+        webView.navigationDelegate = self
+        let url = NSURL(string: "http://getpool.dcoin.club")
+        NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) in
+            guard error == nil else {
+                print("Error \(error?.userInfo)")
+                return
+            }
+            let json = try! NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+            as! [String: String]
+            
+            dispatch_async(dispatch_get_main_queue(), { 
+                let request = NSURLRequest(URL: NSURL(string: json["pool"]!)!)
+                self.webView.loadRequest(request)
+            })
+            
+        }.resume()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +43,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    func webView(webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
+        print("provisional navigation")
+    }
+    
+    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+        let request = navigationAction.request
+        let path = request.URL?.absoluteString
+        
+        if let isThere = path?.containsString("dcoinKey") where isThere {
+            decisionHandler(.Cancel)
+            let str = request.URL!.absoluteString + "&ios=1&first=1"
+            let url = NSURL(string: str)!
+            print("downloading key \(url)")
+            dispatch_async(dispatch_get_main_queue(), {
+                if let data = NSData(contentsOfURL: url) {
+                    if let img = UIImage(data: data) {
+                         UIImageWriteToSavedPhotosAlbum(img, nil, nil, nil)
+                    }
+                }
+            })
+        }
+        
+        decisionHandler(WKNavigationActionPolicy.Allow)
+        print("decidePolicyForNavigationAction")
+    }
 }
 
